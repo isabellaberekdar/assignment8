9c3755787bcfa66d4ed0d3fdd76732feface6045
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-01-10 15:12:20 -0500

    Updates

---
+++ precache-manifest.739f58ffe98f016845d1aef1d208acbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e9fff303015087e73871a1d0261ce9",
+    "url": "/assignment8/index.html"
+  },
+  {
+    "revision": "5962737f8f771ac4b451",
+    "url": "/assignment8/static/css/main.734c29df.chunk.css"
+  },
+  {
+    "revision": "fc426109caafb68e1a48",
+    "url": "/assignment8/static/js/2.b10ec4ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/assignment8/static/js/2.b10ec4ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5962737f8f771ac4b451",
+    "url": "/assignment8/static/js/main.f839e60b.chunk.js"
+  },
+  {
+    "revision": "ae3f09db812fc624845c",
+    "url": "/assignment8/static/js/runtime-main.c05c2930.js"
+  }
+]);
