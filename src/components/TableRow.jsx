3981ea3dd5a658185fc1b3bafd24e85754438c49
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-01-10 22:32:40 -0500

    add-styling

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,23 +1,14 @@
-import React from 'react';
-import TableCell from './TableCell'
-
+import React from "react"
+import TableCell from "./TableCell"
 
 function TableRow(props) {
-    let cells = []
-    for(let i=0; i < props.cols; i++) {
-        cells.push(
-            <TableCell 
-                color={props.color}
-                colorAll={props.colorAll}
-                changeColor={props.changeColor} 
-                 
-            />)
-    }
-    return (
-        <tr>
-            {cells.map(cell => cell)}
-        </tr>
+  let cells = []
+  for (let i = 0; i < props.cols; i++) {
+    cells.push(
+      <TableCell color={props.color} colorAll={props.colorAll} changeColor={props.changeColor} />
     )
+  }
+  return <tr>{cells.map(cell => cell)}</tr>
 }
 
 export default TableRow
