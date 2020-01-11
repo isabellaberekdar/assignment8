dddd09b138b142ac44174eb5470f4726bae5c5fc
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-01-10 22:54:09 -0500

    Added styling: closes #6, #5, #4

--- src/App.js
+++ src/App.js
@@ -13,26 +13,31 @@ class App extends React.Component {
     }
   }
 
+  // Add columns to the grid
   addCol = () => {
     this.setState({ cols: this.state.cols + 1 })
   }
 
+  // Add rows to the grid
   addRow = () => {
     this.setState({ rows: this.state.rows + 1 })
   }
 
+  // Remove rows from the grid
   removeCol = () => {
     if (this.state.cols > 0) {
       this.setState({ cols: this.state.cols - 1 })
     }
   }
 
+  // Remove columns from the grid
   removeRow = () => {
     if (this.state.rows > 0) {
       this.setState({ rows: this.state.rows - 1 })
     }
   }
 
+  // click on a single cell, changing its color to the currently selected color
   changeCellColor = e => {
     const event = e.target
     this.setState({ colorAll: false }, () => {
@@ -40,10 +45,12 @@ class App extends React.Component {
     })
   }
 
+  // Select a color from a dropdown menu of colors
   changeColor = e => {
     this.setState({ color: e.target.value })
   }
 
+  // Fill all uncolored cells with the currently selected color
   clear = e => {
     this.setState({ colorAll: true })
   }
@@ -51,16 +58,18 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <select name='color' onChange={this.changeColor}>
-          <option value='blue'>Blue</option>
-          <option value='cyan'>Cyan</option>
-          <option value='turquoise'>Turquoise</option>
-          <option value='pink'>Pink</option>
-        </select>
-        <button onClick={this.addCol}>Add Column</button>
-        <button onClick={this.addRow}>Add Row</button>
-        <button onClick={this.removeCol}>Remove Col</button>
-        <button onClick={this.removeRow}>Remove Row</button>
+        <div className='buttons'>
+          <select name='color' onChange={this.changeColor}>
+            <option value='blue'>Blue</option>
+            <option value='cyan'>Cyan</option>
+            <option value='turquoise'>Turquoise</option>
+            <option value='pink'>Pink</option>
+          </select>
+          <button onClick={this.addCol}>Add Column</button>
+          <button onClick={this.addRow}>Add Row</button>
+          <button onClick={this.removeCol}>Remove Col</button>
+          <button onClick={this.removeRow}>Remove Row</button>
+        </div>
         <Table
           rows={this.state.rows}
           cols={this.state.cols}
@@ -69,7 +78,7 @@ class App extends React.Component {
           colorAll={this.state.colorAll}
         />
         <button onClick={this.clear}>Clear Cells</button>
-        <button onClick={this.clear}>Color All</button>
+        {/* <button onClick={this.clear}>Color All</button> */}
       </div>
     )
   }
